5ff0ea263070d7175e3383a8b2c825fb0a902f07
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-03-20 19:07:11 -0300

    page setup

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  html: {
+    fontSize: '62.5%',
+  },
+
+  body: {
+   
+  },
+
+  'body, input, textarea, button': {
+
+  },
+})
---
+++ src/styles/index.ts
@@ -0,0 +1,29 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches ({
+  theme: {
+    colors: { 
+      
+    },
+
+    fontSizes: {
+      ss: '1.2rem',
+      sm: '1.4rem',
+      md: '1.8rem',
+      lg: '2.0rem',
+      xl: '2.4rem',
+      '2xl': '3.2rem',
+      '3xl': '3.6rem',
+      '4xl': '4.5rem',
+    }
+  }
+})
